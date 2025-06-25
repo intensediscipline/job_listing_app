b7a9b014b38161a63643a5082213ec486114215b
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-06-25 08:54:23 +0100

    Added hero component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,14 @@
   <title>{{ $title ?? "Workaholics | Find and list jobs" }}</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   @vite('resources/css/app.css')
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 
 <body class="bg-gray-100">
   <x-header />
+  @if(request()->is('/'))
+    <x-hero title="Find Your Dream Job" />
+  @endif
   <main class="container mx-auto p-4 mt-4">
     {{ $slot }}
   </main>
