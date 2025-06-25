e7d69f8f5f1cf62f7d295081540f94fd2e845122
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-06-25 11:57:30 +0100

    Add top and bottom banner components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,9 @@
   <x-header />
   @if(request()->is('/'))
     <x-hero title="Find Your Dream Job" />
+    <x-top-banner />
   @endif
+  
   <main class="container mx-auto p-4 mt-4">
     {{ $slot }}
   </main>
