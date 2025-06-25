4acd289984b1209977be68e86d79ce508fe3839f
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-06-25 22:47:42 +0100

    Defined relationships
    Added more fields to job_listing table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     protected $table = 'job_listings'; // because our job model points to job_listings table
 
-    protected $fillable = ['title', 'description'];
+    protected $fillable = ['title', 'description', 'salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'county', 'postcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website', 'user_id'];
+
+    // relationship to user, jobs belong to a user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
