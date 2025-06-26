931ab104c7323fef39e0baf596ca83de2f8e4b25
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-06-26 18:49:40 +0100

    Created job factory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listings'; // because our job model points to job_listings table
 
     protected $fillable = ['title', 'description', 'salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'county', 'postcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website', 'user_id'];
