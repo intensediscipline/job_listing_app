1fad23df25cc085dd1ab3241a853921d239fbaa0
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-07-02 16:42:29 +0100

    Added alert component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,6 +18,14 @@
   @endif
   
   <main class="container mx-auto p-4 mt-4">
+    {{-- Display success alert message --}}
+    @if(session('success'))
+      <x-alert type="success" message="{{session('success')}}" />
+    @endif
+    {{-- Display error alert message --}}
+    @if(session('error'))
+      <x-alert type="error" message="{{session('error')}}" />
+    @endif
     {{ $slot }}
   </main>
   <script src="{{asset('js/script.js')}}"></script>
