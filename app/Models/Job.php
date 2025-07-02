742eb043998c457994ccce0bd36b44118b5c1787
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-07-02 16:26:35 +0100

    Added validation for save to db

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -13,7 +13,7 @@ class Job extends Model
 
     protected $table = 'job_listings'; // because our job model points to job_listings table
 
-    protected $fillable = ['title', 'description', 'salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'county', 'postcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website', 'user_id'];
+    protected $fillable = ['title', 'description', 'salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'state', 'postcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website', 'user_id'];
 
     // relationship to user, jobs belong to a user
     public function user(): BelongsTo
