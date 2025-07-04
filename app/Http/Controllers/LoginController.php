1585a8aa80adbc192d7dabfdbba108a3edca26c1
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-07-04 23:52:46 +0100

    Ability to login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -11,5 +12,26 @@ class LoginController extends Controller
     public function login(): View
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string'
+        ]);
+
+        //attempt to authenticate user
+        if (Auth::attempt($credentials)) {
+            // regenerate the session to prevent fixation attacks
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'))->with('success', 'You are now logged in!');
+        }
+
+        // if auth fails return back with error
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records'
+        ])->onlyInput('email');
     }
 }
