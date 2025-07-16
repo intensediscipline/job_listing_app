3728387cb4631033900f9a3e5fa357359d6e355e
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-07-16 15:48:55 +0100

    Added application form

--- resources/views/components/inputs/text-area.blade.php
+++ resources/views/components/inputs/text-area.blade.php
@@ -1,4 +1,4 @@
-@props(['id', 'name', 'label' => null, 'placeholder', 'value' => '', 'cols' => '30', 'rows' => '7'])
+@props(['id', 'name', 'label' => null, 'placeholder' => '', 'value' => '', 'cols' => '30', 'rows' => '7'])
 <div class="mb-4">
     @if($label)
         <label class="block text-gray-700" for="{{$id}}">{{$label}}</label>
@@ -9,7 +9,7 @@
         id="{{$id}}"
         name="{{$name}}"
         class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
-        placeholder="{{$placeholder}}"
+        placeholder="{{$placeholder}}"  
     >{{old('$name', $value)}}</textarea>
     @error($name)
         <p class="text-red-500 text-sm mt-1">{{$message}}</p>
