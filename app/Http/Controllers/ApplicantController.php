135e1fae75d8be5183af08b5e25a3bfcb2305cfd
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2025-07-16 19:59:59 +0100

    prevent users applying more than once

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -14,6 +14,14 @@ class ApplicantController extends Controller
     // @route /jobs/{job}/apply
     public function store(Job $job, Request $request)
     {
+        // check if user has already applied
+        // get existng application
+        $existingApplication = Applicant::where('job_id', $job->id)->where('user_id', auth()->user()->id)->exists();
+
+        if ($existingApplication) {
+            return redirect()->back()->with('error', 'You have already applied for this job');
+        }
+
         $validatedData = $request->validate([
             'full_name' => 'required|string',
             'contact_phone' => 'string',
